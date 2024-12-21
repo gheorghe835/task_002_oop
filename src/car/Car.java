71874d61285090a67834ed6a2edf75c99b6305fd
gheorghe835 <gheorghe835@gmail.com>
2024-12-21 09:26:05 +0200

    adaugarea metodelor:pentru accelerare, pentru frânare,pentru afișarea detaliilor despre mașină

--- src/car/Car.java
+++ src/car/Car.java
@@ -12,4 +12,38 @@ public class Car {
         this.speedMax = speedMax;
         this.speedNow = speedNow;
     }
+    // Metodă pentru accelerare
+    public void speedUp(int value){
+        if(value > 0){
+            speedNow += speedMax;
+            if(speedNow > speedMax){
+                speedNow = speedMax;
+            }
+            System.out.println("  Masina a accelerat. Viteza curenta :: " + speedNow + " km/h." );
+        }
+        else{
+            System.out.println("Valoarea pentru accelerare trebuie să fie pozitivă.");
+        }
+    }
+
+      // Metodă pentru frânare
+      public void speedDown(int value){
+        if (value > 0) {
+            speedNow -= value;
+            if (speedNow < 0) {
+                speedNow = 0;
+            }
+            System.out.println("Mașina a frânat. Viteza curentă: " + speedNow);
+        }
+        else{
+            System.out.println(" valoarea pentru frinare trebuie sa fie pozitiva");
+        }
+      }
+
+       // Metodă pentru afișarea detaliilor despre mașină
+       public void showDetails(){
+        System.out.println("  Marca: " + brand + "\n Model: " + model + 
+        "\n Viteza maxima: " + speedMax + " km/h. \n Viteza curenta: " + speedNow);
+       }
+
 }
